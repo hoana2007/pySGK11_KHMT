1055f1e7ca1345ddf8968949dd1da580a46c18d3
Nguyễn Anh Hòa <hoana2007@outlook.com>
2026-03-05 21:58:23 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Code này dành cho các bạn thích tìm hiểu về python trong chương trình Tin học 11 (Khoa học máy tính).
+# pySGKL11_KHMT
--- test.py
+++ test.py
@@ -1,14 +1,41 @@
+#Thuật toán sắp xếp nổi bọt
+# def bubbleSort(A):
+#     n = len(A)
+#     global dem
+#     dem = 0
+#     for i in range(n -1):
+#         for j in range(n -1-i):
+#             if (A[j] > A[j+1]):
+#                 A[j], A[j+1] = A[j + 1], A[j]
+#                 dem += 1
+#TT Quicksort
+def quicksort(A):
+    global dem
+    dem = 0
+    if len(A) < 2:
+        return A
+        dem = 1
+    else:
+        import random
+        pivot = A[random.randint(0, len(A) - 1)]
+        less = [i for i in A if i < pivot]
+        equal_nums = [i for i in A if i == pivot]
+        greater = [i for i in A if i > pivot]
+        dem += 1
+        return quicksort(less) + equal_nums + quicksort(greater)
+
 #Đọc dữ liệu đơn giản
-fname = "dulieu.inp"
+fname = "dayso.inp"
 f = open(fname, "r", encoding="UTF-8")
-#daySo = f.readlines()
-#Đây là 1 danh sách:
-#print(daySo)
-#Đây là đọc từng dòng:
-dayA = []
-for line in f:
-    dayA.append(line)
+
+dayA = [int(x) for x in f.read().split()]
 f.close()
 print(dayA)
-# for i in range(len(dayA)):
-#     print(dayA[i])
+#bubbleSort(dayA)
+quicksort(dayA)
+print(dayA)
+print(f"So buoc hoan doi: {dem}")
+#Kiểm tra dung lượng tạm của file:
+import os
+file_size = os.path.getsize(fname)
+print(f"Kích thước của file: {file_size} bytes")
